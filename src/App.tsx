78701cc7bfe8649b516e929a21d634ff9548a007
dm1nh <dangminhngo.dev@gmail.com>
2025-06-11 22:07:24 +0700

    chore: setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import PWABadge from "./PWABadge.tsx"
 function App() {
   return (
     <>
+      <div className="text-red-500">
+        You can start your Progressive Web App from here.
+      </div>
       <PWABadge />
     </>
   )
